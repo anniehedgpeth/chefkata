0417a3a987c4ccabfe0019da72358bce0e2e1437
Annie Hedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-02 16:52:06 -0600

    directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,4 @@
 include_recipe 'ubuntu'
 include_recipe 'nano'
+
+directory '/var/website'
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,3 +5,11 @@ end
 describe os[:family] do
   it { should eq 'debian' }
 end
+
+describe directory('/var/website') do
+  it { should exist }
+end
+
+describe directory('/var/old-website') do
+  it { should_not exist }
+end
