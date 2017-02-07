75e6851a379cf980d6130cb0d96cf0c768fdd3c3
Annie Hedgpeth <annie@Annies-MacBook-Pro.local>
2017-02-07 10:36:12 +0100

    custom resource

--- resources/ran.rb
+++ resources/ran.rb
@@ -1,13 +1,15 @@
+resource :my_website
+property :git_repo, String, default: 'https://github.com/pages-themes/architect.git', name_property: true
 
-
+action :create do
   execute 'ran' do
     command 'echo ran command > /var/website/command.txt'
     not_if{ ::File.exist?('/var/website/command.txt') }
   end
-  
+
   git '/var/website/architect' do
     repository 'https://github.com/pages-themes/architect.git'
     action :nothing
     subscribes :sync, 'execute[ran]', :immediately
   end
-}
+end
