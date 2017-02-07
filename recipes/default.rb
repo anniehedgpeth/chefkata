449245fb7b78d4ad14a6f364bbf5c00e2a202965
Annie Hedgpeth <annie@Annies-MacBook-Pro.local>
2017-02-07 10:28:35 +0100

    kata through remote file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,27 @@
-#
-# Cookbook:: chefkata
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'ubuntu'
+include_recipe 'nano'
+
+directory '/var/website'
+
+file '/var/website/directions.txt' do
+  content 'website goes here'
+end
+
+file '/var/website/builder.txt' do
+  content "#{node['chefkata']['builder']} built this"
+end
+
+remote_file '/var/website/logo.jpg' do
+  source 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgQmQ0CYwU3cpFE6gEB82cp6TSIcBJSisax_HVvEfsgYHGBsO8kQ'
+end
+
+execute 'ran' do
+  command 'echo ran command > /var/website/command.txt'
+  not_if{ ::File.exist?('/var/website/command.txt') }
+end
+
+git '/var/website/architect' do
+  repository 'https://github.com/pages-themes/architect.git'
+  action :nothing
+  subscribes :sync, 'execute[ran]', :immediately
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,33 @@
-# # encoding: utf-8
-
-# Inspec test for recipe chefkata::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+describe package('nano') do
+  it { should be_installed }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe os[:family] do
+  it { should eq 'debian'}
+end
+
+describe directory('/var/website') do
+  it { should exist }
+end
+
+describe directory('/var/old-website') do
+  it { should_not exist }
+end
+
+describe file('/var/website/builder.txt') do
+  it { should exist }
+  its('content') { should match /Test Kitchen built this/ }
+end
+
+describe file('/var/website/logo.jpg') do
+  it { should exist }
+end
+
+describe file('/var/website/command.txt') do
+  it { should exist }
+  its('content') { should match /ran command/ }
+end
+
+describe directory('/var/website/architect') do
+  it { should exist }
 end
