7497d6a1f0b7004ff288b29e6b94356dc444e0d0
Annie Hedgpeth <annie@Annies-MacBook-Pro.local>
2017-02-08 12:18:59 +0100

    git repo resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,3 +19,9 @@ execute 'ran' do
   command ' echo ran command > /var/website/command.txt'
   not_if { ::File.exist?('/var/website/command.txt') }
 end
+
+git '/var/website/architect' do
+  repository 'https://github.com/pages-themes/architect'
+  action :nothing
+  subscribes :sync, 'execute[ran]', :immediately
+end
