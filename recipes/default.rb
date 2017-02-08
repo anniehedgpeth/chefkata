5196a889aecd70e898552e1536841f01a4b8cfc6
Annie Hedgpeth <annie@Annies-MacBook-Pro.local>
2017-02-08 16:10:34 +0100

    custom resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,5 @@ end
 remote_file '/var/website/logo.jpg' do
   source 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgQmQ0CYwU3cpFE6gEB82cp6TSIcBJSisax_HVvEfsgYHGBsO8kQ'
 end
+
+chefkata 'https://github.com/mhedgpeth/chef-by-example.git'
--- resources/website.rb
+++ resources/website.rb
@@ -1,6 +1,5 @@
-resource_name :git_website
-# Property is a made up word for use in the resource and not in quotes so that you can use it in as variable. In this case, "git_repo" is equivalent to "command" in the execute resource. "name_property" is what is in quotes after the resource is called. If you don't put anything as the property, then it uses what's in the name_property.
-property :git_repo, String, default: 'https://github.com/pages-themes/architect', name_property: true
+resource_name :chefkata
+property :kata, String, name_property: true
 
 action :create do
   execute 'ran' do
@@ -8,9 +7,9 @@ action :create do
     not_if { ::File.exist?('/var/website/command.txt') }
   end
 
-  git 'git-architect' do
-    destination '/var/website/architect'
-    repository git_repo
+  git 'chefkata' do
+    destination '/var/website/chefkata'
+    repository kata
     action :nothing
     subscribes :sync, 'execute[ran]', :immediately
   end
