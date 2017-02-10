3b9f8914875fb4ed460f8e5495e2a39e493a20e5
Annie Hedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-10 11:23:19 -0600

    version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'annie.hedgpeth@gmail.com'
 license 'all_rights'
 description 'Installs/Configures chefkata'
 long_description 'Installs/Configures chefkata'
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/anniehedgpeth/chefkata/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/anniehedgpeth/chefkata' if respond_to?(:source_url)
