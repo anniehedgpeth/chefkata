05280de3024e658250d0f6c12491b2acd3604fca
Annie Hedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-13 21:42:59 -0600

    not finished with notif

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,20 @@
-#
-# Cookbook:: chefkata
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'ubuntu'
+
+directory '/var/website'
+
+file '/var/website/directions.txt' do
+  content 'website goes here'
+end
+
+file '/var/website/builder.txt' do
+  content "#{node['chefkata']['builder']} built this"
+end
+
+remote_file '/var/website/logo.jpg' do
+  source 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgQmQ0CYwU3cpFE6gEB82cp6TSIcBJSisax_HVvEfsgYHGBsO8kQ'
+end
+
+execute 'ran' do
+  command 'echo ran command > /var/website/command.txt'
+  notif File.exist['/var/website/command.txt']
+end
