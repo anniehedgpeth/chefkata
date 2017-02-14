7f9098be67d719620715d2673bbdc399875289be
Annie Hedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-14 09:04:13 -0600

    midway thru kata

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,11 @@
-#
-# Cookbook:: chefkata
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'ubuntu'
+
+directory '/var/website'
+
+file '/var/website/directions.txt' do
+  content 'website goes here'
+end
+
+file '/var/website/builder.txt' do
+  content "#{node['chefkata']['builder']} built this"
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+describe package('nano') do
+  it { should be_installed }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe directory('/var/website') do
+  it { should exist }
+end
+
+describe directory('/var/old-website') do
+  it { should_not exist }
 end
