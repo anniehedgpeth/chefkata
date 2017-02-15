173887d2c34a6825b071bbeb19f903214567d0b8
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-15 12:11:47 -0600

    I'm confused about committing with this current structure

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,48 @@
-# # encoding: utf-8
-
-# Inspec test for recipe chefkata::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+describe os[:family] do
+  it { should eq 'debian' }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe package('nano') do
+  it { should be_installed }
+end
+
+describe directory('/var/website') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe directory('/var/website2') do
+  it { should_not exist }
+end
+
+describe file('/var/website/directions.txt') do
+  it { should exist }
+  its('content') { should eq 'website goes here' }
+end
+
+describe file('/var/website/builder.txt') do
+  it { should exist }
+  its('content') { should eq 'Test Kitchen built this' }
+end
+
+describe file('/var/website/logo.jpg') do
+  it { should exist }
+end
+
+describe file('/var/website/command.txt') do
+  it { should exist }
+  its('content') { should match 'ran command' }
+end
+
+describe directory('/var/website/arch') do
+  it { should exist }
+end
+
+describe file('/var/website/arch/README.md') do
+  it { should exist }
+  its('content') { should match 'The Architect theme' }
+end
+
+describe file('/var/website/arch/jekyll-theme-architect.gemspec') do
+  it { should exist }
 end
