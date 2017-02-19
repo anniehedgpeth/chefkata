2b8c8c8a058e974398aaee42602af36a8d45466a
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-19 16:18:40 -0600

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'annie.hedgpeth@gmail.com'
 license 'all_rights'
 description 'Installs/Configures chefkata'
 long_description 'Installs/Configures chefkata'
-version '0.1.1'
+version '0.1.2'
 
 depends 'ubuntu', '~> 2.0.0'
