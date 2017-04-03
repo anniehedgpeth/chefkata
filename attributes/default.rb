7ad430408cddda5f606d97f2ad4b8ad1a13a8c51
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-03 08:10:20 -0500

    additions for studying, but not tested

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,11 @@
-default['chefkata']['builder'] = 'Annie'
+default['chefkata']['builder']['name'] = 'Annie'
+
 default['chefkata']['archrepo'] = 'https://github.com/pages-themes/architect.git'
+
+default['chefkata'] = {
+  builder: {
+    name: 'michael',
+    address: 'ruby'
+  },
+  archrep: 'something'
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,8 @@ file '/var/website/builder.txt' do
   content "#{node['chefkata']['builder']} built this"
 end
 
+shell_out('cat /etc/logcheck/violations.d/mdadm')
+
 remote_file '/var/website/logo.jpg' do
   source 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgQmQ0CYwU3cpFE6gEB82cp6TSIcBJSisax_HVvEfsgYHGBsO8kQ'
 end
@@ -20,7 +22,15 @@ end
 
 message = data_bag_item('website', 'message')
 welcome = message['welcomeMessage']
+Chef::Log.info("The Welcome Message is '#{message}'")
 
 file '/var/website/welcome.txt' do
   content welcome
+  # sensitive true
+end
+
+# Chef::Log.debug('Finished Compiling')
+
+if node['platform'] == 'ubuntu'
+  Chef::Log.info('This is Ubuntu!')
 end
