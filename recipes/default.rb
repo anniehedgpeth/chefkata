ada5788e256741cf7161a5dca2b07dc1b78cb1ac
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-03 08:46:18 -0500

    converging after attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'nano' do
+  action :install
+end
+
+directory '/var/website' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+file '/var/website/directions.txt' do
+  content 'website goes here'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+file '/var/website/builder.txt' do
+  content "#{node['chefkata']['builder']} built this."
+end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,27 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+# unless os.windows?
+#   describe user('root') do
+#     it { should exist }
+#     skip 'This is an example test, replace with your own test.'
+#   end
+# end
+
+# describe port(80) do
+#   it { should_not be_listening }
+#   skip 'This is an example test, replace with your own test.'
+# end
+
+describe package('nano') do
+  it { should be_installed }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe file('/var/website/builder.txt') do
+  its('content') { should match /Annie built this/ }
 end
+
+describe file('/var/website/directions.txt') do
+  its('content') { should match /website goes here/ }
+end
+
