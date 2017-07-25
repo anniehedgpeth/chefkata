b6461c28467934bdc57d7096a155fcab9a0ec4a5
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-07-25 15:47:50 -0500

    Merge pull request #1 from anniehedgpeth/20170403
    New Starting Point

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'nano'
+
+directory '/var/website'
+
+file '/var/website/directions.txt' do
+  content 'website goes here'
+end
---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,22 @@
+# # encoding: utf-8
+
+# Inspec test for recipe chefkata::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe package('nano') do
+  it { should be_installed }
+end
+
+describe directory('/var/website') do
+  it { should exist }
+end
+
+describe directory('/var/old-website') do
+  it { should_not exist }
+end
+
+describe file('/var/website/directions.txt') do
+  its('content') { should eq 'website goes here' }
+end
